0159c7b9fc63608d58a9db736c999a00a2c5084f
Antonios Karagiannis <antokarag@gmail.com>
2024-01-23 23:18:03 -0700

    feat: add Lcd

---
+++ src/lcd.h
@@ -0,0 +1,14 @@
+#ifndef LCD
+#define LCD
+#include <Arduino.h>
+#include <Wire.h>
+
+class Lcd
+{
+public:
+    // methods
+    static void setup();
+    static void loop();
+};
+
+#endif // LCD
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 #include "device.h"
+#include "lcd.h"
 
 void setup()
 {
     Device::setup();
+    Lcd::setup();
 }
 
 void loop()
 {
     Device::loop();
+    Lcd::loop();
 }
