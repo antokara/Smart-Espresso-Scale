ff0a239ce5b1848c69f5d2efc0ed4b058eadc77b
Antonios Karagiannis <antokarag@gmail.com>
2024-02-03 23:14:30 -0700

    WIP brew

--- src/modes/brew_stages.h
+++ src/modes/brew_stages.h
@@ -3,8 +3,27 @@
 
 enum brew_stages
 {
+    /**
+     * @brief when the timer has not started yet.
+     * depends on start timer.
+     *
+     */
+    brew_stage_waiting,
+    /**
+     * @brief the timer has started
+     *
+     */
     brew_stage_in_progress,
+    /**
+     * @brief the pump has stopped but
+     * if stop timer is set to last drop, it may be still dripping
+     *
+     */
     brew_stage_stopping,
+    /**
+     * @brief no more dripping
+     *
+     */
     brew_stage_done
 };
 
--- src/modes/mode_brew.cpp
+++ src/modes/mode_brew.cpp
@@ -6,12 +6,36 @@
 #include "modes/mode_brew.h"
 #include "services/scale.h"
 
+void Mode_Brew::setStage(brew_stages stage)
+{
+    if (Mode_Brew::stage != stage)
+    {
+        Mode_Brew::stage = stage;
+        Buttons::ignoreAll();
+        Mode_Brew::render();
+        if (stage == brew_stage_stopping)
+        {
+            if (Presets::presets[Mode_SelectPreset::selectedPresetIndex]->stopTimer == stopTimer_pump)
+            {
+                // TODO: stop the brew (toggle the relay switch)
+                Mode_Brew::setStage(brew_stage_done);
+            }
+        }
+    }
+}
+
 void Mode_Brew::setup()
 {
+    Mode_Brew::setStage(brew_stage_waiting);
     // auto-tare right before we start, to negate the cup (that should already be on the scale)
     Scale::tare();
     Mode_Brew::render();
-    // TODO: start the brew (toggle the relay switch)
+    if (Presets::presets[Mode_SelectPreset::selectedPresetIndex]->startTimer == startTimer_pump)
+    {
+        Mode_Brew::setStage(brew_stage_in_progress);
+        // TODO: start the brew (toggle the relay switch)
+        // TODO: start the timer, if set on pump
+    }
 };
 
 void Mode_Brew::loop()
@@ -19,7 +43,16 @@ void Mode_Brew::loop()
     // TODO:
     if (Scale::hasWeightChanged)
     {
+        if (Mode_Brew::stage == brew_stage_waiting && Presets::presets[Mode_SelectPreset::selectedPresetIndex]->startTimer == startTimer_first_drop)
+        {
+            Mode_Brew::setStage(brew_stage_in_progress);
+            // TODO: start the timer, if set on first drop and not already started
+        }
         Mode_Brew::render();
+    }
+    else if (Mode_Brew::stage == brew_stage_in_progress && Presets::presets[Mode_SelectPreset::selectedPresetIndex]->stopTimer == stopTimer_last_drop)
+    {
+        // TODO: detect timeout without a drop and if true, switch stage to done
     }
 };
 
@@ -54,20 +87,6 @@ void Mode_Brew::down()
 
 void Mode_Brew::ok()
 {
-}
-
-void Mode_Brew::setStage(brew_stages stage)
-{
-    if (Mode_Brew::stage != stage)
-    {
-        Mode_Brew::stage = stage;
-        Buttons::ignoreAll();
-        Mode_Brew::render();
-        if (stage == brew_stage_stopping)
-        {
-            // TODO: stop the brew (toggle the relay switch)
-        }
-    }
 }
 
 void Mode_Brew::cancel()
